67d7f2855cc15f7f34601eadef02d29c7e05ed56
Alvin Cherk <alvin@industrialsciences.com.au>
2022-09-20 00:49:57 +1000

    Update week 2

---
+++ tute02/src/employee/Employee.java
@@ -0,0 +1,11 @@
+package employee;
+
+import java.time.LocalDate;
+
+/**
+ * Create an `Employee` class which has private fields for
+ * an employee's name and salary and appropriate getters,
+ * setters, and constructors. Document the class with JavaDoc.
+ */
+public class Employee {
+}
---
+++ tute02/src/employee/Manager.java
@@ -0,0 +1,11 @@
+package employee;
+
+import java.time.LocalDate;
+
+/**
+ * Create a `Manager` class that is a subclass of `Employee`
+ * and has a field for the manager's hire date.
+ */
+public class Manager extends Employee {
+
+}
