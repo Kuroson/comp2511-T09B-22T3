7971a77b2027bd57285772b2997c7bcf92b093b6
Alvin Cherk <alvin@cherk.org>
2022-09-20 14:26:34 +1000

    add week 2 code

--- tute02/src/employee/Employee.java
+++ tute02/src/employee/Employee.java
@@ -1,6 +1,7 @@
 package employee;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 /**
  * Create an `Employee` class which has private fields for
@@ -8,4 +9,54 @@ import java.time.LocalDate;
  * setters, and constructors. Document the class with JavaDoc.
  */
 public class Employee {
+    protected String name;
+    private double salary;
+
+    /**
+     * Creates a new Employee
+     * @param name name of the employee
+     * @param salary salary of the employee
+     */
+    public Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    /**
+     * Gets the employee's name
+     * @return strings of employees name
+     */
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return this.salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + " , " + this.salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) // Equal to itself
+            return true;
+        if ((o.getClass() != this.getClass())) { // Not the same type
+            return false;
+        }
+        Employee employee = (Employee) o; // Typecasting
+        // Comparing the attributes
+        return Objects.equals(name, employee.name) && salary == employee.salary;
+    }
+
 }
--- tute02/src/employee/Manager.java
+++ tute02/src/employee/Manager.java
@@ -7,5 +7,24 @@ import java.time.LocalDate;
  * and has a field for the manager's hire date.
  */
 public class Manager extends Employee {
+    private LocalDate hireDate;
 
+    public Manager(String name, double salary, LocalDate hireDate) {
+        super(name, salary);
+        this.hireDate = hireDate;
+        this.name = "Hello";
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " , " + this.hireDate;
+    }
+
+    public boolean equals(Object o) {
+        if (!super.equals(o)) {
+            return false;
+        }
+        //  check hireDate after
+        return true;
+    }
 }
