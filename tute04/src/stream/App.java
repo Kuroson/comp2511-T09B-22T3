0017d4ff29ad516e3eb5d05abff59291cc352018
Alvin Cherk <alvin@cherk.org>
2022-10-04 15:09:36 +1100

    add

--- tute04/src/stream/App.java
+++ tute04/src/stream/App.java
@@ -2,21 +2,28 @@ package stream;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class App {
     public static void main(String[] args) {
         List<String> strings = new ArrayList<String>(Arrays.asList(new String[] { "1", "2", "3", "4", "5" }));
-        for (String string : strings) {
-            System.out.println(string);
-        }
+        strings.stream().forEach(x -> {
+            System.out.println(x);
+        });
+        // for (String string : strings) {
+        //     System.out.println(string);
+        // }
 
         List<String> strings2 = new ArrayList<String>(Arrays.asList(new String[] { "1", "2", "3", "4", "5" }));
-        List<Integer> ints = new ArrayList<Integer>();
-        for (String string : strings2) {
-            ints.add(Integer.parseInt(string));
-        }
+        List<Integer> ints = strings2.stream().map(x -> {
+            return Integer.parseInt(x);
+        }).collect(Collectors.toList());
+        // List<Integer> ints = new ArrayList<Integer>();
+        // for (String string : strings2) {
+        //     ints.add(Integer.parseInt(string));
+        // }
         System.out.println(ints);
     }
 
